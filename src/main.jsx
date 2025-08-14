6afe1a858e1554b273a184e1f5720040c0c20a44
ibrahem-altayeb <Brh.altayeb@gmail.com>
2025-08-14 19:05:59 +0300

    fix my tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 import GlobalState from "./Components/Context.jsx";
+import "./index.css"; // ✅ Tailwind CSS import
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <GlobalState>
-      <App />
-    </GlobalState>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <GlobalState>
+        <App />
+      </GlobalState>
+    </BrowserRouter>
+  </React.StrictMode>
 );
